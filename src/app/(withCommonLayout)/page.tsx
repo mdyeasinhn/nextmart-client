4576b04aca99c578c56d7775b62e82fdeed71cc6
mdyeasinhn <mdyeasinhn@gmail.com>
2025-09-09 23:09:20 +0600

    Setting and Retrieving Logged-In User Info from Cookies

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,12 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { getCurrentUser } from "@/services/AuthServices";
 
 
-const HomePage = () => {
+const HomePage = async () => {
+  const user = await getCurrentUser();
+  console.log(user)
   return (
     <div>
       <h1>Wellcome to nextmart home page</h1>
       <Button>click me</Button>
-      
+
     </div>
   );
 };
--- src/services/AuthServices/index.ts
+++ src/services/AuthServices/index.ts
@@ -1,7 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use server"
+import { cookies } from "next/headers";
 import { FieldValues } from "react-hook-form"
+import { jwtDecode } from "jwt-decode";
 
+// User Registration
 export const registerUser = async (userData: FieldValues) => {
     try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_BASIC_API}/user`,
@@ -14,11 +17,17 @@ export const registerUser = async (userData: FieldValues) => {
             }
 
         )
-        return res.json();
+        const result = await res.json();
+        if (result.success) {
+            (await cookies()).set("accessToken", result.data.accessToken);
+        }
+        return result;  
     } catch (error: any) {
         return Error(error)
     }
-}
+};
+
+// User Login
 export const loginUser = async (userData: FieldValues) => {
     try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_BASIC_API}/auth/login`,
@@ -29,10 +38,25 @@ export const loginUser = async (userData: FieldValues) => {
                 },
                 body: JSON.stringify(userData)
             }
-
         )
-        return res.json();
+        const result = await res.json();
+        if (result.success) {
+            (await cookies()).set("accessToken", result.data.accessToken);
+        }
+        return result;
     } catch (error: any) {
         return Error(error)
+    }
+};
+
+// Get Current User from Token
+export const getCurrentUser = async () => {
+    const accessToken = (await cookies()).get("accessToken")!.value;
+    let decodedData = null;
+    if (accessToken) {
+        decodedData = await jwtDecode(accessToken)
+        return decodedData;
+    } else {
+        return null
     }
 }
