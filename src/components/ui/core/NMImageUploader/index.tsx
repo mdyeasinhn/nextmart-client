7ba47f43a9e1e02eb99e96af29c2e9bcf0f738fb
mdyeasinhn <mdyeasinhn@gmail.com>
2025-09-11 17:45:50 +0600

    Implementing Image Preview Functionality

--- src/components/ui/core/NMImageUploader/index.tsx
+++ src/components/ui/core/NMImageUploader/index.tsx
@@ -1,13 +1,31 @@
 import { useState } from "react";
 import { Input } from "../../input";
+import Image from "next/image";
+type TImageUploader = {
+    label?: string;
+    className?: string;
+    setImageFiles: React.Dispatch<React.SetStateAction<File[]>>;
+    setImagePreview: React.Dispatch<React.SetStateAction<string[]>>;
+}
 
-const NMImageUploader = () => {
-    const [imageFiles, setImageFiles] = useState<File[] | []>([]);
+const NMImageUploader = ({ imageFiles, setImageFiles }: TImageUploader) => {
+    const [imagePreview, setImagePreview] = useState<string[] | []>([]);
 
     const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files![0];
-        setImageFiles((prev) => [...prev, file])
-    }
+        setImageFiles((prev) => [...prev, file]);
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setImagePreview((prev) => [...prev, reader.result as string]);
+            };
+            reader.readAsDataURL(file);
+        }
+        event.target.value = "";
+    };
+
+
     return (
         <div>
             <Input
@@ -22,6 +40,13 @@ const NMImageUploader = () => {
                 className="w-full h-36 md:size-36 flex items-center justify-center border-2 border-dashed border-gray-300 rounded-md cursor-pointer text-center text-sm text-gray-500 hover:bg-gray-50 transition"
                 htmlFor="image-uploader"
             >Upload Logo</label>
+            <div>
+                {
+                    imagePreview.map((preview, idx) => (
+                        <Image src={preview} alt="image" key={idx} width={500} height={500} />
+                    ))
+                }
+            </div>
         </div>
     );
 };
